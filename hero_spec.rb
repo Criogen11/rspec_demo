e925cae1a3a4d25361f60df0f7978d645426598d
Criogen11 <tsv1985@bk.ru>
2017-12-17 15:05:02 +0300

    add 2 tests

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,5 +1,13 @@
 require './hero'
 
-describe Hero do
+	describe Hero do
+		it "has a capitalized name" do
+			hero = Hero.new 'mike'
+			expect(hero.name).to eq 'Mike' #Аналог hero.name == 'Mike'
+		end	
 
+		it "can power up" do
+			hero = Hero.new 'mike'
+			expect(hero.power_up).to eq 110
+		end	
 end	
